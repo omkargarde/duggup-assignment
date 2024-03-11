fe299499113b246d667a80460fc40348f4b13635
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-03-11 09:55:31 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,6 @@
 
 <Header />
 <slot />
+<!-- https://www.figma.com/file/u1B8DTArPwpnzBcMR04fvH/Timeline---Duggup?type=design&node-id=0-1&mode=design&t=r7s6BmNzJWD7HUuC-0
+ -->
+<!-- https://docs.google.com/document/d/1P65hfxjgcLzAIrKTeMenCvh69NrBJk-jUeamIwBRnyM/edit#heading=h.fuo97qb69ib7 -->
